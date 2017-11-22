ffc831e4349abb5c4eda20e7ab9e1602897959d0
Gurpreet Singh <gdhuper@gmail.com>
2017-11-22 00:00:01 -0800

    seting up envs, initial starter code, data

---
+++ NeuralNetwork.py
@@ -0,0 +1,30 @@
+
+
+
+class NeuralNetwork:
+
+
+	def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
+		self.inodes = inputNodes
+		self.hnodes = hiddenNodes
+		self.onodes = outputNodes
+
+		#learning rate 
+		self.lr = learningRate
+
+
+
+	def train(self):
+		print("stuff")
+
+
+
+	def query(self):
+		pass
+
+
+
+nn = NeuralNetwork("", "", "", "")
+
+
+nn.train()
