7be0f71d3a2855728ebacf6ceab310a1bc207e0b
Gurpreet Singh <gdhuper@gmail.com>
2017-11-27 19:23:26 -0800

    added helper methods

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,5 +1,6 @@
-
-
+import sys
+import math
+import numpy as np
 
 class NeuralNetwork():
 
@@ -22,7 +23,41 @@ class NeuralNetwork():
 		pass
 
 
+	def sigmoid(self, x):
+		return 1 / (1 + math.exp(-x))
 
-nn = NeuralNetwork("", "", "", "")
+	def dotproduct(self, weights, inputs):
+		#dot product using numpy
+		#return np.dot(weights, inputs) 
+		rowsWeights = len(weights)
+		colsWeights = len(weights[0])
 
-nn.train()
+		rowsInputs = len(inputs)
+		colsInputs = len(inputs[0])
+
+		if colsWeights != rowsInputs:
+			return None
+
+		result = [[0 for r in range(colsInputs)] for c in range(rowsWeights)] #initialize result matrix
+
+		for i in range(rowsWeights):
+			for j in range(colsInputs):
+				for k in range(colsWeights):
+					result[i][j] += weights[i][k] * inputs[k][j]
+
+		return result
+
+
+def main():
+	nn = NeuralNetwork("", "", "", "")
+	#print(nn.sigmoid(1.05))
+	# weights = np.matrix('1 2; 3 4')
+	# inputs = np.matrix('5 6; 7 8')
+
+	weights = [[1, 2], [3, 4]]
+	inputs = [[1, 2], [3, 4]]
+	print(nn.dotproduct(weights, inputs))
+
+
+if __name__ == '__main__':
+	main()
